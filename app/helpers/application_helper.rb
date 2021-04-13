e7e7310b2e1ecd5fc8277822e3bfe8be43e95dc2
tmax818 <tylermaxwell661@gmail.com>
2021-04-13 12:10:19 -0700

    stoke kevin's solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 
-    def error_messages_for object
-        render(partial: 'application/error_messages', locals: {object: object})
-    end
+  def error_messages_for(object)
+    render(:partial => 'application/error_messages', :locals => {:object => object})
+  end
 
-    def generate_options_array object
-        object.map {|obj| [obj.name, obj.id.to_i]}
-    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-## Users
+subject = Subject.create!(name: "First Subject", position: 1, visible: true)
+subject2 = Subject.create!(name: "Second Subject", position: 2, visible: true)
 
-User.create!(first_name: "Kevin", last_name: "Skoglund", email: "ks@gmail.com")
-User.create!(first_name: "Tyler", last_name: "Maxwell", email: "tmax818@gmail.com")
+Page.create!(name: "First Page", content: "This is the content of the first page! Sweet sweet content! Oh, yes!", position: 1, visible: true, subject_id: subject.id)
 
-## Subjects
+Page.create!(name: "Second Page", content: "More sweet, sweet content!", position: 2, visible: true, subject_id: subject2.id)
 
-subject1 = Subject.create!(name: "First Subject", position: 1, visible: true)
-subject2 = Subject.create!(name: "Second Subject", position: 2, visible: false)
-
-## Pages
-
-Page.create!(name: "First Page", content: "First page content", position: 1,  visible: false)
-
+Page.create!(name: "Second Page for Second Subject", content: "More sweet, sweet content! You did it!!", position: 2, visible: true, subject_id: subject2.id)
